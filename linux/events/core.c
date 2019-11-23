e1f8140a61eaa69e639f2c0bb2bcf6211eab2a88
ahmadrezamontazerolghaem <ahmadreza.20012@gmail.com>
2019-11-23 21:17:41 +0330

    Update core.c

--- linux/events/core.c
+++ linux/events/core.c
@@ -83,7 +83,6 @@ task_function_call(struct task_struct *p, int (*func) (void *info), void *info)
 		.info	= info,
 		.ret	= -ESRCH, /* No such (running) process */
 	};
-
 	if (task_curr(p))
 		smp_call_function_single(task_cpu(p), remote_function, &data, 1);
 
