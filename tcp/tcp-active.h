d15b8ab56156b5168704559352d8a7d855a512f5
ahmadrezamontazerolghaem <ahmadreza.20012@gmail.com>
2019-11-23 21:48:32 +0330

    Update tcp-active.h

--- tcp/tcp-active.h
+++ tcp/tcp-active.h
@@ -9,7 +9,6 @@ G_BEGIN_DECLS
 NiceSocket * nice_tcp_active_socket_new (GMainContext *ctx, NiceAddress *addr);
 NiceSocket * nice_tcp_active_socket_connect (NiceSocket *socket, NiceAddress *addr);
 
-
 G_END_DECLS
 
 #endif /* _TCP_ACTIVE_H */
