81bc92451dbe118c6876884384d836eb8291263e
ahmadrezamontazerolghaem <ahmadreza.20012@gmail.com>
2019-11-23 21:16:31 +0330

    Update user.c

--- linux/user.c
+++ linux/user.c
@@ -6,8 +6,7 @@
 #include <linux/interrupt.h>
 #include <linux/export.h>
 #include <linux/user_namespace.h>
-#include <linux/proc_ns.h>
-
+#include <linux/proc_ns.h
 /*
  * userns count is 1 for root user, 1 for init_uts_ns,
  * and 1 for... ?
